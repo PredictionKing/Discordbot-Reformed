f597ecf022f8046207c1fd8aee9206c581fd2ec2
PredictionKing <bouzztwix@gmail.com>
2023-04-10 21:44:09 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+public class Main {
+    public static void main(String[] args){
+
+        JDABuilder builder = JDABuilder.createDefault(System.getenv("discord_token"));
+
+        // Disable parts of the cache
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        // Enable the bulk delete event
+        builder.setBulkDeleteSplittingEnabled(false);
+        // Set activity (like "playing Something")
+        builder.setActivity(Activity.watching("TV"));
+
+        builder.build();
+    }
+}

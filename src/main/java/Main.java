fba2480a3c36cd278504074016f5abce38cfaa5b
PredictionKing <bouzztwix@gmail.com>
2023-04-11 10:24:30 +0200

    added Logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main {
@@ -13,7 +16,22 @@ public class Main {
         builder.setBulkDeleteSplittingEnabled(false);
         // Set activity (like "playing Something")
         builder.setActivity(Activity.watching("TV"));
-
+        configureMemoryUsage(builder);
         builder.build();
+    }
+
+    public static void configureMemoryUsage(JDABuilder builder) {
+        // Disable cache for member activities (streaming/games/spotify)
+        builder.disableCache(CacheFlag.ACTIVITY);
+
+        // Only cache members who are either in a voice channel or owner of the guild
+        builder.setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER));
+
+        // Disable member chunking on startup
+        builder.setChunkingFilter(ChunkingFilter.NONE);
+
+        // Consider guilds with more than 50 members as "large".
+        // Large guilds will only provide online members in their setup and thus reduce bandwidth if chunking is disabled.
+        builder.setLargeThreshold(50);
     }
 }
